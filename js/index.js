1b3da32337f199166b62115981ca96b38ac0746c
Robin Criniere <rcriniere@gmail.com>
2022-01-22 20:39:29 -0600

    creates a function that implements a card selector

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-console.log("hello");
+function cardSelector(id) {
+  const cardGroup = document.querySelectorAll(`${id} .uk-card-default`);
+
+  cardGroup.forEach((card) => {
+    card.addEventListener('click', () => {
+      cardGroup.forEach((c) => {
+        c.classList.remove('active');
+      });
+      card.classList.add('active');
+    });
+  });
+}
+cardSelector('#preferences');
+cardSelector('#beanType');
+cardSelector('#quantity');
+cardSelector('#grindOption');
+cardSelector('#deliveries');
